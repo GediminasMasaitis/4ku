b83297c3862f25e1382d9839d3127ccda540ff36
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-11-27 19:42:09 +0200

    LMR history

--- src/main.cpp
+++ src/main.cpp
@@ -804,8 +804,9 @@ i32 alphabeta(Position &pos,
         else {
             // Late move reduction
             i32 reduction = depth > 2 && num_moves_evaluated > 4 && !gain
-                                ? num_moves_evaluated / 13 + depth / 15 + (alpha == beta - 1) + !improving -
-                                      min(max(hh_table[pos.flipped][move.from][move.to] / 128, -2), 2)
+                                ? max(num_moves_evaluated / 13 + depth / 15 + (alpha == beta - 1) + !improving -
+                                          hh_table[pos.flipped][move.from][move.to] / 128,
+                                      -1)
                                 : 0;
 
         zero_window:
