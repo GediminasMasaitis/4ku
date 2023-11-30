c4721ed70de35b0d868e201226ce2cb51f396d05
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-11-30 21:15:51 +0200

    Allow LMR for captures

--- src/main.cpp
+++ src/main.cpp
@@ -799,7 +799,7 @@ i32 alphabeta(Position &pos,
                                hh_table);
         else {
             // Late move reduction
-            i32 reduction = depth > 2 && num_moves_evaluated > 4 && !gain
+            i32 reduction = depth > 2 && num_moves_evaluated > 4
                                 ? max(num_moves_evaluated / 13 + depth / 15 + (alpha == beta - 1) + !improving -
                                           min(max(hh_table[pos.flipped][1][move.from][move.to] / 128, -2), 2),
                                       -1)
