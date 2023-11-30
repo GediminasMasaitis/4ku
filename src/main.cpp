3bcd555c11e4ea03330d4ad631013bf77611678f
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-11-30 22:05:17 +0200

    Adjust move ordering

--- src/main.cpp
+++ src/main.cpp
@@ -742,7 +742,7 @@ i32 alphabeta(Position &pos,
         if (i == !(no_move == tt_move))
             for (i32 j = 0; j < num_moves; ++j) {
                 const i32 gain = max_material[moves[j].promo] + max_material[piece_on(pos, moves[j].to)];
-                move_scores[j] = hh_table[pos.flipped][!gain][moves[j].from][moves[j].to] +
+                move_scores[j] = hh_table[pos.flipped][!gain][moves[j].from][moves[j].to] * 2 +
                                  (gain || moves[j] == stack[ply].killer ? 2048 : 0) + gain;
             }
 
