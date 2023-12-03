437b241db8a4316122209bfa09e920388468c6ec
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-12-03 12:01:13 +0200

    Prevent intermixing killer with captures
    Bench: 5234137

--- src/main.cpp
+++ src/main.cpp
@@ -742,8 +742,10 @@ i32 alphabeta(Position &pos,
         if (i == !(no_move == tt_move))
             for (i32 j = 0; j < num_moves; ++j) {
                 const i32 gain = max_material[moves[j].promo] + max_material[piece_on(pos, moves[j].to)];
-                move_scores[j] = hh_table[pos.flipped][!gain][moves[j].from][moves[j].to] +
-                                 (gain || moves[j] == stack[ply].killer) * 2048 + gain;
+                move_scores[j] = hh_table[pos.flipped][!gain][moves[j].from][moves[j].to] + gain +
+                                 (gain                            ? 2048
+                                  : moves[j] == stack[ply].killer ? 1024
+                                                                  : 0);
             }
 
         // Find best move remaining
