b6b85828cffcc29040679ab6bb24b5dfc4d27245
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-12-06 19:10:49 +0200

    Countermove

--- src/main.cpp
+++ src/main.cpp
@@ -747,7 +747,8 @@ i32 alphabeta(Position &pos,
         if (i == !(no_move == tt_move))
             for (i32 j = 0; j < num_moves; ++j) {
                 const i32 gain = max_material[moves[j].promo] + max_material[piece_on(pos, moves[j].to)];
-                move_scores[j] = hh_table[pos.flipped][!gain][moves[j].from][moves[j].to] +
+                const i32 cont1 = ply && !gain ? continuations[stack[ply - 1].piece][stack[ply - 1].to][piece_on(pos, moves[j].from)][moves[j].to] : 0;
+                move_scores[j] = hh_table[pos.flipped][!gain][moves[j].from][moves[j].to] + cont1 +
                                  (gain || moves[j] == stack[ply].killer) * 2048 + gain;
             }
 
@@ -857,8 +858,11 @@ i32 alphabeta(Position &pos,
             if (score >= beta) {
                 tt_flag = Lower;
 
-                if (!gain)
+                if (!gain) {
                     stack[ply].killer = move;
+                    if(ply)
+                        continuations[stack[ply-1].piece][stack[ply-1].to][piece_on(pos, move.from)][move.to] += depth * depth - depth * depth * continuations[stack[ply - 1].piece][stack[ply - 1].to][piece_on(pos, move.from)][move.to] / 512;
+                }
 
                 hh_table[pos.flipped][!gain][move.from][move.to] +=
                     depth * depth - depth * depth * hh_table[pos.flipped][!gain][move.from][move.to] / 512;
@@ -869,6 +873,10 @@ i32 alphabeta(Position &pos,
                         depth * depth +
                         depth * depth *
                             hh_table[pos.flipped][!prev_gain][moves_evaluated[j].from][moves_evaluated[j].to] / 512;
+                    if(!prev_gain) {
+                        if(ply)
+                            continuations[stack[ply - 1].piece][stack[ply - 1].to][piece_on(pos, moves_evaluated[j].from)][moves_evaluated[j].to] -= depth * depth + depth * depth * continuations[stack[ply - 1].piece][stack[ply - 1].to][piece_on(pos, moves_evaluated[j].from)][moves_evaluated[j].to] / 512;
+                    }
                 }
                 break;
             }
