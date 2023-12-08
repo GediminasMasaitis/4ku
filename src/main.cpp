e87ee1fc02e89960a4ff7cb6fe55003d1b447fd2
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-12-08 02:48:32 +0200

    LMR adjust
    Bench: 4646788

--- src/main.cpp
+++ src/main.cpp
@@ -803,7 +803,8 @@ i32 alphabeta(Position &pos,
             // Late move reduction
             i32 reduction = depth > 2 && num_moves_evaluated > 4
                                 ? max(num_moves_evaluated / 13 + depth / 15 + (alpha == beta - 1) + !improving -
-                                          min(max(hh_table[pos.flipped][!gain][move.from][move.to] / 128, -2), 2),
+                                          min(max(hh_table[pos.flipped][!gain][move.from][move.to], -1), 1) +
+                                          hh_table[pos.flipped][!gain][move.from][move.to] / 256,
                                       0)
                                 : 0;
 
