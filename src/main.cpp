cad30263be82a56dab4c0b53900df72a54c1f940
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-12-14 00:16:44 +0200

    Pawn attack tuning
    Bench: 5307786

--- src/main.cpp
+++ src/main.cpp
@@ -422,19 +422,19 @@ void generate_piece_moves(Move *const movelist,
 }
 
 const i32 phases[] = {0, 1, 1, 2, 4, 0};
-const i32 max_material[] = {137, 443, 446, 837, 1693, 0, 0};
-const i32 material[] = {S(93, 137), S(325, 443), S(334, 446), S(440, 837), S(750, 1693), 0};
+const i32 max_material[] = {137, 444, 447, 838, 1699, 0, 0};
+const i32 material[] = {S(91, 137), S(318, 444), S(327, 447), S(430, 838), S(726, 1699), 0};
 const i32 pst_rank[] = {
     0,         S(-3, 0),  S(-3, -1), S(-1, -1), S(1, 0),  S(5, 2), 0,        0,          // Pawn
-    S(-3, -4), S(-1, -3), S(0, -1),  S(2, 2),   S(3, 3),  S(6, 1), S(4, 0),  S(-12, 1),  // Knight
-    S(-1, -1), S(2, -1),  S(2, 0),   S(2, 0),   S(2, 1),  S(3, 0), 0,        S(-8, 2),   // Bishop
+    S(-2, -4), S(-1, -3), S(0, -1),  S(2, 2),   S(3, 3),  S(6, 1), S(4, 0),  S(-12, 1),  // Knight
+    S(-1, -2), S(2, -1),  S(2, 0),   S(2, 0),   S(2, 1),  S(2, 0), 0,        S(-8, 2),   // Bishop
     S(0, -3),  S(-1, -3), S(-2, -2), S(-3, 0),  S(0, 2),  S(2, 2), S(1, 3),  S(3, 1),    // Rook
-    S(2, -11), S(3, -8),  S(1, -4),  S(0, 1),   S(-1, 5), S(0, 5), S(-3, 6), S(-1, 5),   // Queen
+    S(2, -11), S(3, -8),  S(1, -4),  S(0, 1),   S(-1, 5), S(0, 5), S(-3, 6), S(-2, 5),   // Queen
     S(-1, -5), S(1, -2),  0,         S(-2, 2),  S(0, 4),  S(5, 4), S(2, 3),  S(1, -3)    // King
 };
 const i32 pst_file[] = {
     S(-1, 1),  S(-2, 1),  S(-1, 0), S(0, -1), S(1, 0),  S(2, 0),  S(2, 0),  S(-1, -1),  // Pawn
-    S(-5, -3), S(-2, -1), S(0, 1),  S(2, 3),  S(2, 2),  S(2, 0),  S(1, 0),  S(-1, -3),  // Knight
+    S(-4, -3), S(-2, -1), S(0, 1),  S(2, 3),  S(2, 2),  S(2, 0),  S(1, 0),  S(-1, -3),  // Knight
     S(-2, 0),  0,         S(1, 0),  S(0, 1),  S(1, 1),  S(-1, 1), S(2, 0),  S(0, -1),   // Bishop
     S(-2, 0),  S(-1, 1),  S(0, 1),  S(1, 0),  S(2, -1), S(1, 0),  S(1, 0),  S(-2, 0),   // Rook
     S(-2, -4), S(-1, -2), S(-1, 0), S(0, 1),  S(0, 2),  S(1, 2),  S(2, 1),  S(1, -1),   // Queen
@@ -442,30 +442,31 @@ const i32 pst_file[] = {
 };
 const i32 open_files[] = {
     // Semi open files
-    S(1, 5),
+    S(2, 5),
     S(-5, 19),
-    S(18, 15),
-    S(3, 18),
+    S(17, 15),
+    S(2, 18),
     S(-21, 9),
     // Open files
     S(-3, -11),
     S(-11, 0),
-    S(46, 0),
+    S(45, 1),
     S(-15, 37),
     S(-59, 1),
 };
-const i32 mobilities[] = {S(9, 5), S(8, 7), S(3, 4), S(4, 2), S(-5, 0)};
-const i32 king_attacks[] = {S(10, -4), S(19, -5), S(27, -9), S(20, 5), 0};
-const i32 pawn_protection[] = {S(22, 14), S(2, 14), S(6, 17), S(8, 9), S(-5, 19), S(-30, 24)};
-const i32 pawn_threat_penalty[] = {S(-4, 1), S(21, 2), S(11, 6), S(10, 17), S(9, 16), S(5, 5)};
-const i32 passers[] = {S(3, 14), S(33, 50), S(63, 124), S(195, 253)};
-const i32 pawn_passed_protected = S(10, 18);
+const i32 mobilities[] = {S(8, 5), S(8, 7), S(3, 4), S(4, 2), S(-5, 0)};
+const i32 king_attacks[] = {S(10, -4), S(19, -5), S(26, -9), S(20, 5), 0};
+const i32 pawn_protection[] = {S(22, 14), S(2, 14), S(6, 17), S(8, 9), S(-6, 19), S(-31, 24)};
+const i32 pawn_attacked[] = {S(58, 31), S(62, 46), S(54, 23), S(59, 15), 0};
+const i32 pawn_threat_penalty[] = {S(-4, 1), S(20, 2), S(11, 6), S(10, 17), S(9, 16), S(5, 5)};
+const i32 passers[] = {S(3, 14), S(32, 51), S(61, 125), S(195, 255)};
+const i32 pawn_passed_protected = S(9, 18);
 const i32 pawn_doubled_penalty = S(10, 36);
 const i32 pawn_phalanx = S(12, 12);
-const i32 pawn_passed_blocked_penalty[] = {S(8, 13), S(-7, 40), S(-9, 82), S(-10, 158)};
+const i32 pawn_passed_blocked_penalty[] = {S(9, 13), S(-7, 40), S(-8, 82), S(-7, 160)};
 const i32 pawn_passed_king_distance[] = {S(1, -6), S(-4, 11)};
 const i32 bishop_pair = S(32, 71);
-const i32 king_shield[] = {S(35, -11), S(27, -6)};
+const i32 king_shield[] = {S(35, -10), S(27, -6)};
 const i32 pawn_attacked_penalty[] = {S(63, 14), S(156, 140)};
 
 [[nodiscard]] i32 eval(Position &pos) {
@@ -539,9 +540,7 @@ const i32 pawn_attacked_penalty[] = {S(63, 14), S(156, 140)};
                 } else {
                     // Pawn attacks
                     if (piece_bb & attacked_by_pawns)
-                        // If we're to move, we'll just lose some options and our tempo.
-                        // If we're not to move, we lose a piece?
-                        score -= pawn_attacked_penalty[c];
+                        score -= pawn_attacked[p - 1];
 
                     u64 mobility = 0;
 
